8f8104c10a0fe8e932969f0b64f7af48a8a67cbf
manimovassagh <mani.mgh@gmail.com>
2021-08-11 22:31:09 +0200

    delete added

--- src/main/java/com/github/manimovassagh/RestAPISpring/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/github/manimovassagh/RestAPISpring/dao/EmployeeDAOHibernateImpl.java
@@ -15,21 +15,45 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
     private EntityManager entityManager;
 
+    @Autowired
+    public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
     @Override
-    @Transactional
     public List<Employee> findAll() {
         Session currentSession = entityManager.unwrap(Session.class);
         //create a query
         Query<Employee> theQuery =
                 currentSession.createQuery("From Employee", Employee.class);
-
         List<Employee> employees = theQuery.getResultList();
-
         return employees;
     }
 
-    @Autowired
-    public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
-        entityManager = theEntityManager;
+
+    @Override
+    public Employee findById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        //get the employee
+        Employee theEmployee = currentSession.get(Employee.class, theId);
+        return theEmployee;
     }
+
+    @Override
+    public void save(Employee theEmployee) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(theEmployee);
+
+
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Employee> theQuery = currentSession.createQuery("DELETE From Employee WHERE id=:employee", Employee.class);
+        theQuery.setParameter("employeeId", theId);
+        theQuery.executeUpdate();
+    }
+
+
 }

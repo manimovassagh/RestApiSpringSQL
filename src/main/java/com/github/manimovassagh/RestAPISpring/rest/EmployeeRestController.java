a83482ff42ee8e4b51e5baaae675e73ec9547bf4
manimovassagh <mani.mgh@gmail.com>
2021-08-11 23:18:34 +0200

    api for one id added

--- src/main/java/com/github/manimovassagh/RestAPISpring/rest/EmployeeRestController.java
+++ src/main/java/com/github/manimovassagh/RestAPISpring/rest/EmployeeRestController.java
@@ -3,11 +3,10 @@ package com.github.manimovassagh.RestAPISpring.rest;
 
 import com.github.manimovassagh.RestAPISpring.dao.EmployeeDAO;
 import com.github.manimovassagh.RestAPISpring.entity.Employee;
+import com.github.manimovassagh.RestAPISpring.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,16 +14,30 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
     }
 
     @GetMapping("/employees")
-    public List<Employee> findAll(){
-        return employeeDAO.findAll();
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee findById(@PathVariable int id) {
+        Employee theEmployee = employeeService.findById(id);
+        if (theEmployee == null) {
+            throw new RuntimeException("The Given Employee With id Number " + id + " Not Exist !");
+        }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public void addEmployee(Employee employee) {
+        employeeService.save(employee);
     }
 
 
